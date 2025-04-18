678ada4c7e23480229053016e2a732f0589af22d
Dreamaker <161083130+Dream98789@users.noreply.github.com>
2025-04-18 15:26:21 +0800

    Update booking.js

--- booking.js
+++ booking.js
@@ -7,3 +7,16 @@ if (showFormBtn && notionFormContainer) {
     notionFormContainer.style.display = 'block';
   });
 }
+
+// 顯示感謝訊息
+function showThankYou() {
+  notionFormContainer.innerHTML = '<div class="thank-you-message" style="text-align:center;padding:2em 0;font-size:1.5em;color:#ff9800;">感謝您的預約！</div>';
+}
+
+// 監聽 NoteForms 的 postMessage
+window.addEventListener('message', function(event) {
+  // NoteForms 會發送 {event: 'formSubmission'}
+  if (event.data && event.data.event === 'formSubmission') {
+    showThankYou();
+  }
+});
